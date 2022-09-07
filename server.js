69cc031898e77af10adc346589671bc1c3d2bb8c
VienNC <congvien.01@gmail.com>
2022-09-07 21:13:33 +0700

    video5

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello Word! </h3>');
-    res.write('<h2>From VienNC</h2>');
-    res.end();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello Word!')
 })
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+
+app.get('/about', (req, res) => {
+    res.send('Hello Word!')
 })
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
